2dd6d109bc20d1e73b5398407d319ad48f7316d2
Clara Moreira <anacacamm@gmail.com>
2022-12-17 18:21:06 -0300

    adicionar item na lista

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// Seleção de elementos
+
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+const eraseBtn = document.querySelector("#erase-button");
+const filterBtn = document.querySelector("#filter-select");
+
+// Funções
+const saveTodo = (text) => {
+
+    // PRIMEIRA FORMA DE CRIAR HTML
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = `<i class="fa-solid fa-check"></i>`
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = `<i class="fa-solid fa-pen"></i>`
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+    deleteBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+    text = ''
+}
+
+
+// Eventos
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    inputValue = todoInput.value
+
+    if (inputValue) {
+        saveTodo(inputValue)
+    }
+})
