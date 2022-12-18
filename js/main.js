5185e44c79cca744b04ecc37a8974bc10c55176b
Clara Moreira <anacacamm@gmail.com>
2022-12-18 20:16:26 -0300

    adicionar função de pesquisa

--- js/main.js
+++ js/main.js
@@ -51,17 +51,31 @@ const toggleForms = () => {
 
 const updateTodo = (text) => {
 
-    const todos = document.querySelectorAll('.todo')
+    const todos = document.querySelectorAll('.todo');
 
     todos.forEach((todo) => {
 
-        let todoTitle = todo.querySelector("h3")
+        let todoTitle = todo.querySelector("h3");
 
         if (todoTitle.innerText === oldInputValue) {
-            todoTitle.innerText = text
+            todoTitle.innerText = text;
+        };
+    });
+};
+
+const getSearchedTodos = (search) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+        todo.style.display = "flex";
+
+        if (!todoTitle.includes(search)) {
+            todo.style.display = "none";
         }
-    })
-}
+    });
+};
 
 
 // Eventos
@@ -72,8 +86,8 @@ todoForm.addEventListener('submit', (e) => {
 
     if (inputValue) {
         saveTodo(inputValue);
-    }
-})
+    };
+});
 
 document.addEventListener("click", (e) => {
     const targetEl = e.target;
@@ -98,13 +112,13 @@ document.addEventListener("click", (e) => {
 
     if (targetEl.classList.contains("remove-todo")) {
         parentEl.remove();
-    }
-})
+    };
+});
 
 cancelEditBtn.addEventListener("click", (e) => {
     e.preventDefault();
     toggleForms();
-})
+});
 
 editForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -115,4 +129,18 @@ editForm.addEventListener("submit", (e) => {
     }
 
     toggleForms();
-})
+});
+
+searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value;
+
+    getSearchedTodos(search);
+});
+
+eraseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    searchInput.value = "";
+
+    searchInput.dispatchEvent(new Event("keyup"));
+});
