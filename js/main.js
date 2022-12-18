51da916839ac21e0d586883a53967b38e8c51aa8
Clara Moreira <anacacamm@gmail.com>
2022-12-18 18:41:32 -0300

    editar item da lista

--- js/main.js
+++ js/main.js
@@ -10,10 +10,11 @@ const searchInput = document.querySelector("#search-input");
 const eraseBtn = document.querySelector("#erase-button");
 const filterBtn = document.querySelector("#filter-select");
 
+let oldInputValue;
+
 // Funções
 const saveTodo = (text) => {
 
-    // PRIMEIRA FORMA DE CRIAR HTML
     const todo = document.createElement("div");
     todo.classList.add("todo");
 
@@ -42,37 +43,76 @@ const saveTodo = (text) => {
     todoInput.focus();
 }
 
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+
+    const todos = document.querySelectorAll('.todo')
+
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3")
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text
+        }
+    })
+}
+
 
 // Eventos
 todoForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    inputValue = todoInput.value
+    inputValue = todoInput.value;
 
     if (inputValue) {
-        saveTodo(inputValue)
+        saveTodo(inputValue);
     }
 })
 
 document.addEventListener("click", (e) => {
-    const targetEl = e.target
-    const parentEl = targetEl.closest("div")
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
 
     if (targetEl.classList.contains("finish-todo")) {
-        parentEl.classList.toggle("done")
+        parentEl.classList.toggle("done");
     }
 
     if (targetEl.classList.contains("edit-todo")) {
-        editForm.classList.remove("hide")
-        todoForm.classList.add("hide")
-    }
+        toggleForms()
 
-    if (targetEl.classList.contains("cancel-edit-btn")) {
-        console.log("Cancelar!")
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
     }
 
     if (targetEl.classList.contains("remove-todo")) {
-        parentEl.remove()
-        console.log("Excluiu!")
+        parentEl.remove();
     }
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForms();
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const editInputValue = editInput.value;
+
+    if (editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms();
 })
